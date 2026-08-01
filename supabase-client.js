e465d10ea1a6c53bc99a03f4fd4f21ded8e681da
dosbaevu <hbittracker@gmail.com>
2026-08-01 16:51:18 +0600

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -6,8 +6,8 @@
 // 3. Paste your Project URL and anon public key below
 // ============================================
 
-const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL"; // e.g. https://xxxxx.supabase.co
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://ecumkjiqbdvooebkhsvz.supabase.co"; // e.g. https://xxxxx.supabase.co
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdW1ramlxYmR2b29lYmtoc3Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NzM0NjMsImV4cCI6MjEwMTE0OTQ2M30.bMo-3jWKk2mAvYMuK9gpbHhQpR0KNIsOW_gHV5YJg0A";
 
 const supabaseClient = (SUPABASE_URL !== "YOUR_SUPABASE_PROJECT_URL")
   ? supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
